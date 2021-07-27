7121e57525813900883fe7ce7856b882c7c538ec
“Sudaraka <sudarakayasindu@gmail.com>
2021-07-27 21:01:51 +0800

    Add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,42 +13,41 @@ const AboutPage = ({ data }, location) => {
 
   return (
     <Layout siteMetadata={siteMetadata}>
-      <SEO title="ABout" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
 
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            Clean, minimal, and deeply customisable. London is a theme made for
-            people who appreciate simple lines.
+            About Me
           </h2>
+          {/* <h3 id="dynamic-styles">About Me</h3> */}
+          <p>
+            Photography is the best way to freeze the precious moments of your
+            life. And it will bring back the exact emotion you had even after
+            ages. Since I am a memory collector in my personal life, I am more
+            than happy to help people to collect their wonderful memories in
+            their day-to-day lives.
+          </p>
+          <p>
+            Hello! I am Vishaka, a female freelance photographer based in
+            Singapore. I am a creative person who is keen on colors, shapes,
+            people, and nature. Photography was one of my hobbies since
+            childhood. If you like my work, trust me I will help you to create
+            and explore the best stories about you. I am friendly, flexible, and
+            reliable when it comes to working with clients.
+          </p>
+          <p>
+            Please do not hesitate to contact me if you need to keep magical
+            moments in your life.
+          </p>
           <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.benchAccounting.childImageSharp.fluid}
-              className="kg-image"
+            <img
+              src={
+                "https://live.staticflickr.com/65535/51339612450_fe6d97ab83_k.jpg"
+              }
             />
-            <figcaption>Large imagery is at the heart of this theme</figcaption>
+            <figcaption>It's Just Me</figcaption>
           </figure>
-          <h3 id="dynamic-styles">Dynamic styles</h3>
-          <p>
-            London comes with photo-centric main layout best suited to
-            photography, graphics portfolios and other image-heavy uses.
-          </p>
-          <p>
-            Both post and page templates are light and minimal, with all the
-            focus on the content while the design of the theme gets out of the
-            way. Beneath the hood, London enjoys the full power of the{" "}
-            <a href="https://docs.ghost.org/api/handlebars-themes/">
-              Ghost Handlebars Theme API
-            </a>{" "}
-            to provide limitless customisation options and dynamic styles.
-          </p>
-          <p>
-            Don't forget to check out the{" "}
-            <a href="https://docs.ghost.org/integrations/">
-              Ghost Integrations Directory
-            </a>{" "}
-            for more ways to integrate Ghost with your favourite services.
-          </p>
         </div>
       </article>
     </Layout>
